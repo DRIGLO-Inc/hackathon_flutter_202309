0748fa58bc99531a6144ea5b92a9f37cf97d18ad
Zudah228 <rezel95.hin@gmail.com>
2023-09-30 17:55:41 +0900

    feat: モックの削除

--- lib/presentation/pages/main/widgets/drawer.dart
+++ lib/presentation/pages/main/widgets/drawer.dart
@@ -7,7 +7,6 @@ import '../../../../utils/extensions/navigator_state_ex.dart';
 import '../../../providers/theme_switcher_notifier.dart';
 import '../../../widgets/buttons/theme_switcher_button.dart';
 import '../../../widgets/snack_bars/floating_snack_bar.dart';
-import '../../_mock_chat_room/_mock_chat_room.dart';
 import '../../auth_sign_in/auth_sign_in_page.dart';
 
 class MainPageDrawer extends ConsumerWidget {
@@ -63,12 +62,6 @@ class MainPageDrawer extends ConsumerWidget {
                       horizontal: 12,
                     ),
                     child: Text('開発用'),
-                  ),
-                  ListTile(
-                    title: const Text('チャットルーム'),
-                    onTap: () {
-                      Navigator.of(context).push(MockChatRoom.route());
-                    },
                   ),
                 ],
               ),
