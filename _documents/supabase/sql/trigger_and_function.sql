3f60c9a9e601ed096864234ecf9bb248a4824d71
MIYAMOTO610 <smiyamoto.610@gmail.com>
2023-10-01 11:24:00 +0900

    docs: DB 関数の追加

--- _documents/supabase/sql/trigger_and_function.sql
+++ _documents/supabase/sql/trigger_and_function.sql
@@ -18,6 +18,21 @@ CREATE TRIGGER on_auth_user_created
 AFTER INSERT ON auth.users
 FOR EACH ROW EXECUTE FUNCTION initialization();
 
+-- ランダムに問題を10個取得
+CREATE FUNCTION get_random_questions()
+RETURNS SETOF questions
+LANGUAGE plpgsql
+SECURITY DEFINER
+AS $$
+BEGIN
+    RETURN QUERY
+    SELECT *
+    FROM questions
+    ORDER BY random()
+    LIMIT 10;
+END;
+$$;
+
 -- 問題を更新したとき
 -- CREATE TRIGGER on_question_updated
 -- before update on questions
