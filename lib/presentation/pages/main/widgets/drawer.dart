f4dccd95b140c3d277ebf6b35cd143c4514d7ef9
Zudah228 <rezel95.hin@gmail.com>
2023-10-01 09:19:09 +0900

    feat: Drower のスタイル変更

--- lib/presentation/pages/main/widgets/drawer.dart
+++ lib/presentation/pages/main/widgets/drawer.dart
@@ -4,8 +4,6 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../../../../domain/auth/use_cases/sign_out/sign_out.dart';
 import '../../../../infrastructure/package_info/package_info.dart';
 import '../../../../utils/extensions/navigator_state_ex.dart';
-import '../../../providers/theme_switcher_notifier.dart';
-import '../../../widgets/buttons/theme_switcher_button.dart';
 import '../../../widgets/snack_bars/floating_snack_bar.dart';
 import '../../auth_sign_in/auth_sign_in_page.dart';
 
@@ -57,25 +55,18 @@ class MainPageDrawer extends ConsumerWidget {
                       child: const Text('ログアウト'),
                     ),
                   ),
-                ],
+                ]
+                    .indexed
+                    .map(
+                      (record) => Padding(
+                        padding: EdgeInsets.only(
+                          top: record.$1 != 0 ? 8 : 0,
+                        ),
+                        child: record.$2,
+                      ),
+                    )
+                    .toList(),
               ),
-            ),
-            const Spacer(),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const ThemeSwitcherButton(),
-                const SizedBox(width: 16),
-                Tooltip(
-                  message: '端末のテーマに合わせる',
-                  child: OutlinedButton(
-                    onPressed: () {
-                      ref.read(themeSwitcherNotifierProvider.notifier).reset();
-                    },
-                    child: const Icon(Icons.monitor),
-                  ),
-                ),
-              ],
             ),
           ],
         ),
